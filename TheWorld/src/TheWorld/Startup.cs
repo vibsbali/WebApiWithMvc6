e00be4d3cd1db27417ea534360eba4582fc20c5d
Vaibhav Bali <vibsbali@gmail.com>
2016-04-17 23:10:32 +1000

    Added app.UseStaticFiles()

--- TheWorld/src/TheWorld/Startup.cs
+++ TheWorld/src/TheWorld/Startup.cs
@@ -16,6 +16,8 @@ namespace TheWorld
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            app.UseStaticFiles();
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync($"Hello World! {context.Request.Path}");
