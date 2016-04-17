6575e646a0ff84ef6b334bd792adb3a34a9eeb07
Vaibhav Bali <vibsbali@gmail.com>
2016-04-17 22:38:04 +1000

    Removed app.UseIISPlatformHandler(); middleware as it is used for IIS and we are going to work with bare minimum

--- TheWorld/src/TheWorld/Startup.cs
+++ TheWorld/src/TheWorld/Startup.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
@@ -20,8 +17,6 @@ namespace TheWorld
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
-            app.UseIISPlatformHandler();
-
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
@@ -29,6 +24,9 @@ namespace TheWorld
         }
 
         // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+        public static void Main(string[] args)
+        {
+            WebApplication.Run<Startup>(args);
+        } 
     }
 }
