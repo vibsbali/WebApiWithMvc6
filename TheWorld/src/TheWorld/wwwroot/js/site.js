fb3eb0083d34618edb5efa718544923e10d506f5
Vaibhav Bali <vibsbali@gmail.com>
2016-04-18 13:14:57 +1000

    Added jquery code and style to hide and show sidebar

--- TheWorld/src/TheWorld/wwwroot/js/site.js
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -1,15 +1,37 @@
 ﻿"use strict";
 
 (function () {
-    var ele = document.getElementById("username");
-    ele.innerHTML = "Vibs Bali";
 
-    var main = document.getElementById("main");
-    main.onmouseenter = function () {
-        main.style = "background-color: #888;";
-    };
+    //var ele = $("#username");
+    //ele.text("Vibs Bali");
 
-    main.onmouseleave = function () {
-        main.style = "";
-    };
+    //var main = $("#main");
+    //main.on("mouseenter",
+    //    function () {
+    //        main.css("background-color", "#888");
+    //    });
+
+    //main.on("mouseleave",
+    //    function () {
+    //        main.css("background-color", "");
+    //    });
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click",
+    //    function() {
+    //        var me = $(this);
+    //        alert(me.text());
+    //    });
+
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function() {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+            });
+    
 }())
