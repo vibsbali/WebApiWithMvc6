b3edff81acc64097248d6b0f0228ca9efb8ad5ce
Vaibhav Bali <vibsbali@gmail.com>
2016-04-18 17:13:57 +1000

    Added a new ViewModel for ContactUs Page

---
+++ TheWorld/src/TheWorld/ViewModels/ContactViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheWorld.ViewModels
+{
+    public class ContactViewModel
+    {
+        [Required]
+        [StringLength(255, MinimumLength = 5)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(1024, MinimumLength = 5)]
+        public string Message { get; set; }
+    }
+}
