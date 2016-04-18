2d6f9487f36214de2b672022d78cd624056cf9ec
Vaibhav Bali <vibsbali@gmail.com>
2016-04-18 16:48:53 +1000

    Added TagHelpers to display anchor tags however it will not work until a _ViewImports.cshtml is added

--- TheWorld/src/TheWorld/Controllers/Web/AppController.cs
+++ TheWorld/src/TheWorld/Controllers/Web/AppController.cs
@@ -12,5 +12,15 @@ namespace TheWorld.Controllers.Web
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
     }
 }
