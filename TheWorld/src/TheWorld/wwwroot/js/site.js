ee01718dc3a540715ac227a6c21937e1475bd81f
Vaibhav Bali <vibsbali@gmail.com>
2016-04-18 12:10:34 +1000

    Added onmouseenter and onmouseleave events

--- TheWorld/src/TheWorld/wwwroot/js/site.js
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -1,6 +1,15 @@
 ﻿"use strict";
 
-(function() {
+(function () {
     var ele = document.getElementById("username");
     ele.innerHTML = "Vibs Bali";
+
+    var main = document.getElementById("main");
+    main.onmouseenter = function () {
+        main.style = "background-color: #888;";
+    };
+
+    main.onmouseleave = function () {
+        main.style = "";
+    };
 }())
