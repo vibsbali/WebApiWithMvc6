e4c6a2f3552d6a3643d2e289517f3212516aae4b
Vaibhav Bali <vibsbali@gmail.com>
2016-04-18 17:59:48 +1000

    Adding code that allows us to get environment settings from a json file

--- TheWorld/src/TheWorld/Startup.cs
+++ TheWorld/src/TheWorld/Startup.cs
@@ -1,17 +1,36 @@
 ﻿using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Hosting;
 using Microsoft.AspNet.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using TheWorld.Services;
 
 namespace TheWorld
 {
     public class Startup
     {
+
+        //This is a pattern that allows you to get environment variables 
+        public static IConfigurationRoot Configuration;
+
+        public Startup(IApplicationEnvironment appEnv)
+        {
+            var builder = new ConfigurationBuilder()
+                            .SetBasePath(appEnv.ApplicationBasePath)
+                            .AddJsonFile("config.json")
+                            .AddEnvironmentVariables();
+
+            Configuration = builder.Build();
+        }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddScoped<IMailService, DebugMailService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
