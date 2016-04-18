8a8200843eb79ec377375a18d88f8833f4bae7c1
Vaibhav Bali <vibsbali@gmail.com>
2016-04-18 16:25:32 +1000

    Added Index.cshtml file and default route inside Startup.cs

--- TheWorld/src/TheWorld/Startup.cs
+++ TheWorld/src/TheWorld/Startup.cs
@@ -11,11 +11,19 @@ namespace TheWorld
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app)
         {
+            app.UseMvc();
+
+            app.UseMvc(config =>
+            {
+                config.MapRoute("Default", "{controller}/{action}/{id?}", new {controller = "App", action = "Index"});
+            });
+            
             //This will look for default files type like index.html 
             app.UseDefaultFiles();
             app.UseStaticFiles();
