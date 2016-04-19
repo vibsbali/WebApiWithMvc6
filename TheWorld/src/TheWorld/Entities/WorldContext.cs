b24f3e182fa66a3843658cdca77ff1e7226c0f75
Vaibhav Bali <vibsbali@gmail.com>
2016-04-19 14:02:15 +1000

    Added new configuration in config.json for ConnectionStrings and referenced the connection string from inside the controller using Startup.Configuration

--- TheWorld/src/TheWorld/Entities/WorldContext.cs
+++ TheWorld/src/TheWorld/Entities/WorldContext.cs
@@ -7,5 +7,12 @@ namespace TheWorld.Entities
     {
         public DbSet<Trip> Trips { get; set; }
         public DbSet<Stop> Stops { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var connectionString = Startup.Configuration["Data:ConnectionString"];
+            optionsBuilder.UseSqlServer(connectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
     }
 }
