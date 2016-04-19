e90eb63685c3f8a46f1056e581d7f7a889de6ee2
Vaibhav Bali <vibsbali@gmail.com>
2016-04-20 00:06:37 +1000

    Created a new ViewModel for Api Controller

---
+++ TheWorld/src/TheWorld/ViewModels/TripViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TheWorld.ViewModels
+{
+    public class TripViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
