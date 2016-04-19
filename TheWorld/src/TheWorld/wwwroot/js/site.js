048c2a6c9117b8d8fb907fda083b178b5a7ad6a2
Vaibhav Bali <vibsbali@gmail.com>
2016-04-19 12:19:57 +1000

    Added fa for fontawesome instead of show and hide menu text

--- TheWorld/src/TheWorld/wwwroot/js/site.js
+++ TheWorld/src/TheWorld/wwwroot/js/site.js
@@ -24,13 +24,16 @@
     //    });
 
     var $sidebarAndWrapper = $("#sidebar,#wrapper");
+    var $icon = $("#sidebarToggle i.fa");
 
     $("#sidebarToggle").on("click", function() {
         $sidebarAndWrapper.toggleClass("hide-sidebar");
         if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
-            $(this).text("Show Sidebar");
+            $icon.addClass("fa-angle-right");
+            $icon.removeClass("fa-angle-left");
         } else {
-            $(this).text("Hide Sidebar");
+            $icon.addClass("fa-angle-left");
+            $icon.removeClass("fa-angle-right");
         }
             });
     
