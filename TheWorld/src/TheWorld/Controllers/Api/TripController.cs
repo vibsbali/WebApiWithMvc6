021953cc1c42d70e8b67bf8662881a92882cb93f
Vaibhav Bali <vibsbali@gmail.com>
2016-04-19 23:47:07 +1000

    Added Repository and retured Trip data

--- TheWorld/src/TheWorld/Controllers/Api/TripController.cs
+++ TheWorld/src/TheWorld/Controllers/Api/TripController.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
+using TheWorld.Repository;
 
 // For more information on enabling Web API for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace TheWorld.Controllers.Api
 {
-    
     public class TripController : Controller
     {
+        private readonly IRepository repository;
+
+        public TripController(IRepository repository)
+        {
+            this.repository = repository;
+        }
+
         [HttpGet("api/trips")]
         public JsonResult Get()
         {
-            return Json(new {name = "Shawn"});
+            return Json(repository.GetAllTrips().ToList());
         }
     }
 }
