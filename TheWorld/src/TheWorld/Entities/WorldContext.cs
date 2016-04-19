c973cafd7c794620a4db57b9ac26f9164b2350d5
Vaibhav Bali <vibsbali@gmail.com>
2016-04-19 13:39:35 +1000

    Added EF 7 and Dbcontext

---
+++ TheWorld/src/TheWorld/Entities/WorldContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Data.Entity;
+using TheWorld.Models;
+
+namespace TheWorld.Entities
+{
+    public class WorldContext : DbContext
+    {
+        public DbSet<Trip> Trips { get; set; }
+        public DbSet<Stop> Stops { get; set; }
+    }
+}
