7482b5a08845fdfc306677491d53dba999ebaad4
Vaibhav Bali <vibsbali@gmail.com>
2016-04-19 13:15:32 +1000

    Added Entities for EF

---
+++ TheWorld/src/TheWorld/Models/Trip.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TheWorld.Models
+{
+    public class Trip
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+        public string Username { get; set; }
+
+        public ICollection<Stop> Stops { get; set; }
+    }
+}
